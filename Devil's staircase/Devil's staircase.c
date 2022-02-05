9a0658c4129e6c437187ac85aa19229fe52c2b70
JuneK06 <ka0wa6ju1n9-en314@ezweb.ne.jp>
2022-02-05 14:47:53 +0900

    初期

---
+++ Devil's staircase/Devil's staircase.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+#define N 100
+
+int main(void)
+{
+	double a[N];
+
+	int i;
+
+	for (i = 0; i < N; i++)
+		a[i] = 1.0 / 2;
+
+	for (i = 0; i < N; i++)
+		printf("%d %.3f\n", i, a[i]);
+
+	return 0;
+}
